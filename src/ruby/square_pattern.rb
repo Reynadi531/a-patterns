1398b501e1021bd5c089e338e2909493bccb0d5e
Ike Bai <ike@symbiot.technology>
2020-10-02 14:48:48 +1000

    added square pattern in ruby

---
+++ src/ruby/square_pattern.rb
@@ -0,0 +1,17 @@
+def square_pattern(n)
+    for i in 1..n do
+        for j in 1..n do
+            print('*')
+        end
+        puts('')
+    end
+end
+
+square_pattern(5)
+
+# ruby squarepattern.rb
+#   *****
+#   *****
+#   *****
+#   *****
+#   *****
