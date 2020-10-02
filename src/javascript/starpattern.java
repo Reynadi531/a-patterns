603158a71ab18e17ea883668319f607d3f3b4d18
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-10-02 09:06:17 +0530

    Create starpattern.java

---
+++ src/javascript/starpattern.java
@@ -0,0 +1,34 @@
+public class Starpattern
+{ 
+    public static void pattern(int n) 
+    { 
+        int i, j;  
+        for(i=0; i<n; i++)              
+        {
+        for(j=2*(n-i); j>=0; j--)        
+            {           
+                System.out.print(" ");     
+            } 
+            for(j=0; j<=i; j++)          
+            {       
+                System.out.print("* ");     
+            }           
+            System.out.println();        
+        } 
+    } 
+    
+    public static void main(String args[]) 
+    { 
+        int n = 5; 
+        pattern(n); 
+    } 
+}
+
+
+//
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ //
