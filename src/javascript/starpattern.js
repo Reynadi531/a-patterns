26ccb65400d79793e884d060d7d7e38c45a7b663
Eduardo Vieira <gceduvieira@gmail.com>
2020-10-02 00:27:50 -0400

    add nova funçao

--- src/javascript/starpattern.js
+++ src/javascript/starpattern.js
@@ -1,23 +1,6 @@
-function letDraw(i){
-	let str = ''; r = 1;
-	switch (i) {
-		case 1:
-			str = "*       *";
-			break;
-		case 2:
-			str = "* *   *";
-			break;
-		default:
-			str = "* ";
-			r = i
-			break;
-	}
-	console.log(str.repeat(r))
-}
-
 function starpattern(n) {
 	for(let i=0; i<=n; i++) {
-		letDraw(i)
+		console.log("* ".repeat(i))
 	}
 }
 
@@ -25,11 +8,9 @@ starpattern(5)
 
 /*
  starpattern(5)
- 
-*       *
-* *   *
-* * * 
-* * * * 
-* * * * *
- 
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
  */
