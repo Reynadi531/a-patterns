92a97584c7b048d9e6232bec22d1d48fcd4eb972
Nidhi Nivedita <56748276+NidhiNivedita6@users.noreply.github.com>
2020-10-02 20:58:05 +0530

    Create hollow-square-pattern.c

---
+++ src/C/hollow-square-pattern.c
@@ -0,0 +1,34 @@
+//Hollow Square Pattern
+
+#include<stdio.h>
+int main()
+{
+     int i, j, N;
+    printf("Enter number of rows: ");
+    scanf("%d", &N);
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=N; j++)
+        {
+            if(i==1 || i==N || j==1 || j==N)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//Output:
+Enter number of rows: 5
+*****
+*   *
+*   *
+*   *
+*****
