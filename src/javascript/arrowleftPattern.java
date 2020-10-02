4bf8f8bb0b9e84e52d93062a6f5f749a9b015c66
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-10-02 09:43:06 +0530

    Create arrowleftPattern.java

---
+++ src/javascript/arrowleftPattern.java
@@ -0,0 +1,39 @@
+public class ArrowLeftPattern
+{ 
+    public static void pattern(int n) 
+    { 
+        int i, j;  
+        for(i=1; i<n+1; i++)              
+        {
+        for(j=2*(n-i); j>=0; j--)        
+            {           
+                System.out.print(" ");     
+            } 
+        for(j=0; j<i; j++)          
+        {       
+           System.out.print("* ");     
+         }
+         if(i == 1) {
+              System.out.print("      *");
+         }
+        if(i == 2) {
+              System.out.print("  *");
+        }
+         System.out.println();        
+        } 
+    } 
+    
+    public static void main(String args[]) 
+    { 
+        int n = 5; 
+        pattern(n); 
+    } 
+}
+
+/*
+*       *
+* *   *
+* * * 
+* * * * 
+* * * * * 
+*/
