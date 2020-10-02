29f5b650b7c0bf5538ba25796c10a12c40e56db6
salonigoyal2309 <salonigoyal2309@gmail.com>
2020-10-02 11:50:32 +0530

    feat: Add hollow triangle pattern #34

---
+++ src/C++/hollowStarPattern.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printHollowStarPattern(int n) {
+	/* suppose n is 5 the function will print
+	 *
+	 **
+	 * *
+	 *  *
+	 *****
+	*/
+	for(int i = 1 ; i <= n ; i++) { // i depicts the line number of the pattern
+		for(int j = 1 ; j <= i ; j++) { // j is the number of stars in every line
+			if(i==n || j==1 || j==i) cout << "*" ;
+			else cout<<" ";
+		}
+		cout << "\n"; // a line break after every line
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	printHollowStarPattern(n);
+}
+
+
+
+
