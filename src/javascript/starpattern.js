63ddf273e3acd85120449098bfc9be011e94b7cb
Eduardo Vieira <gceduvieira@gmail.com>
2020-10-02 00:05:10 -0400

    Update starpattern.js
    add a function to that 'letDraw'

--- src/javascript/starpattern.js
+++ src/javascript/starpattern.js
@@ -1,6 +1,23 @@
+function letDraw(i){
+	let str = ''; r = 1;
+	switch (i) {
+		case 1:
+			str = "*       *";
+			break;
+		case 2:
+			str = "* *   *";
+			break;
+		default:
+			str = "* ";
+			r = i
+			break;
+	}
+	console.log(str.repeat(r))
+}
+
 function starpattern(n) {
 	for(let i=0; i<=n; i++) {
-		console.log("* ".repeat(i))
+		letDraw(i)
 	}
 }
 
@@ -8,9 +25,11 @@ starpattern(5)
 
 /*
  starpattern(5)
- *
- * *
- * * *
- * * * *
- * * * * *
+ 
+*       *
+* *   *
+* * * 
+* * * * 
+* * * * *
+ 
  */
