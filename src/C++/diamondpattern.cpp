e82425af3b3b23a85ddd411c561a1744784096f8
Dimas Kurniawan <dimaskurniawan40@gmail.com>
2020-10-02 11:50:12 +0700

    Add one space sperate star symbols and show output

--- src/C++/diamondpattern.cpp
+++ src/C++/diamondpattern.cpp
@@ -14,22 +14,35 @@ int main()
 
     {
         for (j = 0; j < (n - i); j++)
-            cout << " ";
+            cout << "  ";
         for (j = 1; j <= i; j++)
-            cout << "*";
+            cout << " *";
         for (k = 1; k < i; k++)
-            cout << "*";
+            cout << " *";
         cout << "\n";
     }
     for (i = n - 1; i >= 1; i--)
     {
         for (j = 0; j < (n - i); j++)
-            cout << " ";
+            cout << "  ";
         for (j = 1; j <= i; j++)
-            cout << "*";
+            cout << " *";
         for (k = 1; k < i; k++)
-            cout << "*";
+            cout << " *";
         cout << "\n";
     }
     return 0;
 }
+
+/*
+diamond pattern input 5
+         *
+       * * *
+     * * * * *
+   * * * * * * *
+ * * * * * * * * *
+   * * * * * * *
+     * * * * *
+       * * *
+         *
+*/
