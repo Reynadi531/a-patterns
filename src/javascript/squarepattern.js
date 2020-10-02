78fc17bf95681a35fe09e9bd7e482db84aeb0898
Shashank Mishra <geekyhoic2275@gmail.com>
2020-10-02 10:00:54 +0530

    add square pattern using js

---
+++ src/javascript/squarepattern.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      process.stdout.write('* ');
+    }
+    console.log('');
+  }
+  
+  // output:
+  // * * * * *
+  // * * * * *
+  // * * * * *
+  // * * * * *
+  // * * * * *
+  
