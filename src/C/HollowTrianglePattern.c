9de7c8d39788674dc54a0aaf2d775e5376db8a01
Ashish garg <57340919+ashish-garg18@users.noreply.github.com>
2020-10-03 12:27:14 +0530

    Create HollowTrianglePattern.c
    Issue #34 Added hollowTrianglePattern in c language

---
+++ src/C/HollowTrianglePattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,rows;//variable declaration input row value from user
+    printf("Enter of rows you want\n");
+    scanf("%d",&rows);
+    for(i=1; i<=rows; i++){//parent for loop, for row of hollow triangle
+        for(j=1; j<=i; j++){//print columns
+            if(j==1 || j==i || i==rows){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");//move to next line
+    }
+    getch();
+    return 0;
+}
