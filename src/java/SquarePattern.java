1c6e57292e6eda3bf9472af9e168a2b34942098b
Koustav Ghosh <ghosh.hardkots.koustav1@gmail.com>
2020-10-04 13:40:26 +0530

    updating output as comment for Java
    Updating SqurePattern.java

--- src/java/SquarePattern.java
+++ src/java/SquarePattern.java
@@ -19,3 +19,21 @@ public class SquarePattern {
         }
     }
 }
+
+/*
+-> javac SquarePattern.java
+-> java SquarePattern
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* 
+
+*/
