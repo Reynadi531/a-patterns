c95c6c20adcaf20a723286fb2e7efc10f075e83f
TeeVenDick <56723862+TeeVenDick@users.noreply.github.com>
2020-10-07 09:34:00 +0800

    Update squarePattern.m

--- src/Matlab/squarePattern.m
+++ src/Matlab/squarePattern.m
@@ -2,14 +2,14 @@ array = input('\nNumber of rows: ');
 
 for loop = 1:array
     for loop2 = 1:array
-        fprintf('*')
+        fprintf('* ')
     end
     fprintf('\n')
 end
 
 %Sample output if row = 5
-%*****
-%*****
-%*****
-%*****
-%*****
+%* * * * *
+%* * * * *
+%* * * * *
+%* * * * *
+%* * * * *
