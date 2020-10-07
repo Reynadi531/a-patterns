8d0642e2014bd176738f2eb2ac9b351c6938d9ba
TeeVenDick <56723862+TeeVenDick@users.noreply.github.com>
2020-10-07 09:30:19 +0800

    Update diamondPattern.m

--- src/Matlab/diamondPattern.m
+++ src/Matlab/diamondPattern.m
@@ -2,29 +2,29 @@ array = input('\nNumber of rows: ');
 
 for loop = 1:array
     for loop2 = 1:(array - loop)
-        fprintf(' ')
+        fprintf('  ')
     end
     
     for loop2 = 1:(2 * loop - 1)
-        fprintf('*')
+        fprintf('* ')
     end
     fprintf('\n')
 end
 
 for loop = 1:(array -1)
     for loop2 = 1:(loop)
-        fprintf(' ')
+        fprintf('  ')
     end
     
     for loop2 = 1:(2 * (array - loop) - 1)
-        fprintf('*')
+        fprintf('* ')
     end
     fprintf('\n')
 end
 
 %Sample output if row = 3
-%  *
-% ***
-%*****
-% ***
-%  *
+%    *
+%  * * *
+%* * * * *
+%  * * *
+%    *
