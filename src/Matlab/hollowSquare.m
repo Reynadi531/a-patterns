4b46cf6ff611af0ead739cacc001aa24f8828631
TeeVenDick <56723862+TeeVenDick@users.noreply.github.com>
2020-10-07 09:31:46 +0800

    Update hollowSquare.m

--- src/Matlab/hollowSquare.m
+++ src/Matlab/hollowSquare.m
@@ -3,17 +3,17 @@ array = input('\nNumber of rows: ');
 for loop = 1:array
     for loop2 = 1:array
         if(loop == 1 || loop2 == 1 || loop2 == array  || loop == array)
-            fprintf('*')
+            fprintf('* ')
         else
-            fprintf(' ')
+            fprintf('  ')
         end
     end
     fprintf('\n')
 end
 
 %Sample output if row = 5
-%*****
-%*   *
-%*   *
-%*   *
-%*****
+%* * * * *
+%*       *
+%*       *
+%*       *
+%* * * * *
